b1fb22f7848f7dacd0e5ad721ded7761bfb32e66
Oscar V <oscar.vanderhorstvalette@wmich.edu>
2018-11-15 16:28:09 -0500

    Bad implementation of Solution O(n^3), but hey, it works

--- Homework5/MichiganTravel/michigantravel.py
+++ Homework5/MichiganTravel/michigantravel.py
@@ -13,116 +13,111 @@ class Graph(object):
         self._queue = queue.Queue()
         self._visited = []
 
-    def addEdges(self, vert1, vert2, speed):
-        self._graph[vert1].append((vert2,speed))
-        self._graph[vert2].append((vert1,speed))
+    def startVertex(self, vert, ratio):
+        #Ratio
+        self._graph[vert].append([])
+        #Edges
+        self._graph[vert].append([])
 
-    def addRatio(self, vert, ratio):
-        self._graph[vert].append([ratio, ratio])
+    def addEdges(self, vert1, vert2, speed):
+        self._graph[vert1][1].append((vert2,speed))
+        self._graph[vert2][1].append((vert1,speed))
+
+    def addRatio(self, vert, max, min):
+        if ([max, min] not in self._graph[vert][0]):
+            self._graph[vert][0].append([max, min])
+
+    def setRatio(self, vert, vert2, speed):
+        #print(self._graph[vert][0][0])
+
+        #If minimum is bigger than speed...
+        if (self._graph[vert2][0][0][1] > speed):
+            self.addRatio(vert, self._graph[vert2][0][0][0], speed)
+        #If the maximum is smaller than speed...
+        elif (self._graph[vert2][0][0][0] < speed):
+            self.addRatio(vert, speed, self._graph[vert2][0][0][1])
+        else:
+            self.addRatio(vert, self._graph[vert2][0][0][0], self._graph[vert2][0][0][1])
 
     def getVertex(self, id):
         return self._graph[id]
 
-    def checkRatio(self, vert1, vert2, speed):
-        if (self._graph[vert1][0][0] == 0 or self._graph[vert2][0][0] == 0):
-            return True
+    def checkSourceChildren(self, source, end, second):
+        for vert in self._visited:
+            #Look for all vertices next to this vertex.
+            #For those that do not have a path to Source, add them to the queue if second round
+            #For those that have a path to Source, calculate Ratios
+            for edges in self._graph[vert][1]:
+                if (edges[0] in self._visited):
+                    #print(vert, " has ", edges[0])
+                    self.setRatio(vert, edges[0], edges[1])
+                else:
+                    if (second and edges[0] != source and edges[0] != end):
+                        self._queue.put(edges[0])
 
-        toCheckRatio = self._graph[vert1][0][0]/self._graph[vert1][0][1]
-        if (speed > self._graph[vert1][0][0]):
-            toCheckRatio = speed/self._graph[vert1][0][1]
-        #if it is smalles than the MIN
-        elif (speed < self._graph[vert1][0][1]):
-            toCheckRatio = self._graph[vert1][0][0]/speed
+    def checkOtherVertices(self, vert, end):
+        for edges in self._graph[vert][1]:
+            #If the vertex at the end of the edge have paths to the source...
+            allPaths = self._graph[edges[0]][0]
+            if (len(allPaths) > 0):
+                #Get the best path considering the speed we go to given vertex
+                bestRatio = [30001, 1]
+                for ratio in allPaths:
+                    #First change min/max respectively
+                    if (ratio[0] < edges[1]):
+                        ratio = [edges[1], ratio[1]]
+                    elif (ratio[1] > edges[1]):
+                        ratio = [ratio[0], edges[1]]
 
-        #If new ratio is smaller than the ratio that has been seen before...
-        return toCheckRatio < (self._graph[vert2][0][0]/self._graph[vert2][0][1])
+                    if (ratio[0]/ratio[1] < bestRatio[0]/bestRatio[1]):
+                        bestRatio = ratio
 
-    def setRatio(self, vert, vert2, speed):
-        if (self._graph[vert][0][0] == 0):
-            self._graph[vert][0][0] = speed
-            self._graph[vert][0][1] = speed
-            #if it is bigger than the MAX
-            if (speed > self._graph[vert][0][0]):
-                self._graph[vert][0][0] = speed
-            #if it is smalles than the MIN
-            elif (speed < self._graph[vert][0][1]):
-                self._graph[vert][0][1] = speed
-            #if it is bigger than the MAX
-            elif (self._graph[vert2][0][0] > self._graph[vert][0][0]):
-                self._graph[vert][0][0] = self._graph[vert2][0][0]
-            #if it is smalles than the MIN
-            elif ((self._graph[vert2][0][1] < self._graph[vert][0][1]) and self._graph[vert2][0][0] != 0):
-                self._graph[vert][0][1] = self._graph[vert2][0][1]
-            else:
-                None
-        else:
-            #if it is bigger than the MAX
-            if (speed < self._graph[vert][0][0]):
-                self._graph[vert][0][0] = speed
-            #if it is smalles than the MIN
-            elif (speed > self._graph[vert][0][1]):
-                self._graph[vert][0][1] = speed
-            #if it is bigger than the MAX
-            elif (self._graph[vert2][0][0] < self._graph[vert][0][0]):
-                self._graph[vert][0][0] = self._graph[vert2][0][0]
-            #if it is smalles than the MIN
-            elif ((self._graph[vert2][0][1] > self._graph[vert][0][1]) and self._graph[vert2][0][0] != 0):
-                self._graph[vert][0][1] = self._graph[vert2][0][1]
-            else:
-                None
+                #print(bestRatio)
+                self.addRatio(vert, bestRatio[0], bestRatio[1])
 
-    def goToEndCity(self, id1, id2):
-        #Base case for recursion
-        if (id1 == id2):
-            return
+    def findFromEnd(self, end):
+        answer = [30001, 1]
+        for edges in self._graph[end][1]:
+            allPaths = self._graph[edges[0]][0]
+            #Get the best path considering the speed we go to given vertex
+            bestRatio = [30001, 1]
+            for ratio in allPaths:
+                #First change min/max respectively
+                if (ratio[0] < edges[1]):
+                    ratio = [edges[1], ratio[1]]
+                elif (ratio[1] > edges[1]):
+                    ratio = [ratio[0], edges[1]]
 
-        vert1 = self.getVertex(id1)
-        vert2 = self.getVertex(id2)
-        #Move along all edges of vert1 
-        print("\n------------------- For Vertex: " + str(id1))
-        print("---------- My current ratio is: " + str(vert1[0]))
-        for edges in vert1[1:]:
-            #Don't go to cities you came from
-            if (edges[0] in self._visited):
-                continue
+                if (ratio[0]/ratio[1] < bestRatio[0]/bestRatio[1]):
+                    bestRatio = ratio
 
-            #From id1 to edges[0] in edges[1] speed
-            print("From " + str(id1) + " to " + str(edges[0]) + " in speed: " + str(edges[1]))
+            if (bestRatio[0]/bestRatio[1] < answer[0]/answer[1]):
+                    answer = bestRatio
 
-            #Check if current Ratio is better than the ratio the city has
-            if (self.checkRatio(id1, edges[0], edges[1])):
-                print("--- Better ratio!")
-                print("Before Ratio:", self.getVertex(edges[0])[0])
-                self.setRatio(edges[0], id1, edges[1])
-                 #Enqueue the vertex we need to visit in our queue
-                self._queue.put(edges[0])
-            else:
-                print("Worst or Equal Ratio. No changes.")
-
-            print("Ratio of City: " + str (edges[0]))
-            print("\t", self.getVertex(edges[0])[0])
-
-        #Check queue to see if there are more vertices to Check
-        try:
-            next = self._queue.get()
-        except:
-            #No more vertices to look at
-            return
-
-        #Otherwise call function on next vertex
-        self._visited.append(id1)
-        self.goToEndCity(next, id2)
-        print(id1)
-        return
-
-    def backTrackCities(self):
-        print(self._graph)
+        return answer
 
     def findSmoothestPath(self, id1, id2):
-        self.goToEndCity(id1, id2)
-        self.backTrackCities()
+        #Look at all vertices directly attached to the Source
+        for i in self.getVertex(id1)[1]:
+            self.addRatio(i[0], i[1], i[1])
+            self._visited.append(i[0])
+            #self._queue.put(i[0])
+
+        #Look at each of those vertices and look for routes that will end up going to the Source
+        #DO THIS TWICE
+        self.checkSourceChildren(id1, id2, False)
+        self.checkSourceChildren(id1, id2, True)
         
-        #If there are more vertices
+        #As long as there are vertices to check...
+        while (self._queue.qsize()):
+            try:
+                vert = self._queue.get()
+                self.checkOtherVertices(vert, id2)
+            except:
+                break
+
+        #Once all the vertices have been checked, find the smoothest path from vertices adjacent to the end city
+        return self.findFromEnd(id2)
 
     #For printing purposes
     def __str__(self):
@@ -141,7 +136,7 @@ def main():
 
     #Add empty ratio to the cities
     for i in range(1, N+1):
-        myGraph.addRatio(i, 0)
+        myGraph.startVertex(i, 0)
 
     #Get road connections
     for i in range(0, M):
@@ -151,7 +146,12 @@ def main():
     #Get the roads we want to find the smoother connection from
     endGoal = [int(x) for x in list(f.readline().split(" "))]
     
-    myGraph.findSmoothestPath(1, 6)
+    #print(myGraph._graph)
+    result = myGraph.findSmoothestPath(endGoal[0], endGoal[1])
+    if (result[0] == 30001):
+        print("IMPOSSIBLE")
+    else:
+        print(result[0], "/", result[1])
 
 
 #If called not as a module
